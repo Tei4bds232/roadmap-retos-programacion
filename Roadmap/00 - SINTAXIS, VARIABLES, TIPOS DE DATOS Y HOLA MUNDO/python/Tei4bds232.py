f3bbd46f5d36d511e454e09111e138ca17393cab
Tei4bds232 <159572489+Tei4bds232@users.noreply.github.com>
2025-06-13 18:07:19 -0500

    Create Tei4bds232.py
    Solución Ejercicio #00 Retos de programación Mouredev

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Tei4bds232.py
@@ -0,0 +1,30 @@
+"""
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+"""
+#URL del sitio web oficial del lenguaje: https://www.python.org/
+#comentario de una linea
+"""
+comentario de varias lineas
+"""  
+Variable = "Python"
+CONSTANTE = 3.14159 # Pi, por ejemplo
+
+#datos primitivos
+cadena_texto = "Hola, Python"
+entero = 42
+booleano = True
+flotante = 3.14
+none_type = None
+
+#Imprimir por terminal
+var_1 = "Python_3.10"
+print (f"Hola, {var_1}!")
+print(f"¡Hola, {var_1}!")
